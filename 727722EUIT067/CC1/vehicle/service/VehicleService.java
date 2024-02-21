5da71656c704ed82da1d67ff8ba4816c7722b735
Helvinrosen <124854797+Helvinrosen@users.noreply.github.com>
2024-02-21 10:19:24 +0530

    Add files via upload

---
+++ 727722EUIT067/CC1/vehicle/service/VehicleService.java
@@ -0,0 +1,46 @@
+package com.example.vehicle.service;
+
+import java.util.*;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.vehicle.model.Vehicle;
+import com.example.vehicle.repo.VehicleRepo;
+
+@Service
+public class VehicleService {
+    @Autowired
+    VehicleRepo VehicleRepo;
+
+    public Vehicle addVehicle(Vehicle Vehicle)
+    {
+        return VehicleRepo.save(Vehicle);
+    }
+    public List<Vehicle> getVehicle()
+    {
+        return VehicleRepo.findAll();
+    }
+    public Optional<Vehicle> findUser(Long id)
+    {
+        return VehicleRepo.findById(id);
+    }
+    public Vehicle edituser(Long id,Vehicle entity){
+        Vehicle v=VehicleRepo.findById(id).orElse(null);
+        if(v!=null){
+            v.setCustomerName(entity.getCustomerName());
+            v.setCustomerPhNo(entity.getCustomerPhNo());
+            v.setCustomerCar(entity.getCustomerCar());
+            v.setCarNumber(entity.getCarNumber());
+            return VehicleRepo.saveAndFlush(v);
+
+        }   
+        return null;
+    }
+    public String Delete(Long id){
+        VehicleRepo.deleteById(id);
+        return ("Deleted Successfully");
+    }
+    
+}
